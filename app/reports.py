70742549b9bc9f443b25ad90ee8cd9687ffb20ef
Aaron Kimbre <aronwk.aaron@gmail.com>
2022-03-03 11:18:22 -0600

    make reports look at vault items

--- app/reports.py
+++ app/reports.py
@@ -66,7 +66,7 @@ def gen_item_report():
                         attr_prefix="attr_"
                     )
                     for inv in character_json["obj"]["inv"]["items"]["in"]:
-                        if "i" in inv.keys() and type(inv["i"]) == list and (int(inv["attr_t"])==0 or int(inv["attr_t"])==0):
+                        if "i" in inv.keys() and type(inv["i"]) == list and (int(inv["attr_t"])==0 or int(inv["attr_t"])==1):
                             for item in inv["i"]:
                                 if item["attr_l"] in report_data:
                                     report_data[item["attr_l"]] = report_data[item["attr_l"]] + int(item["attr_c"])
