d06bad4641eca273bcc6bc342cf17a77f42147a6
aronwk-aaron <aronwk.aaron@gmail.com>
2022-10-16 22:16:27 -0500

    hotfix

--- app/characters.py
+++ app/characters.py
@@ -234,7 +234,7 @@ def upload(id):
         character_data.xml_data = form.char_xml.data
         character_data.save()
         flash("You accept all consequences from these actions", "danger")
-        log_audit(f"Updated {character_data.name}'s xml data")
+        log_audit(f"Updated {character_data.id}'s xml data")
         return redirect(url_for('characters.view', id=id))
     form.char_xml.data = character_data.xml_data
     return render_template("character/upload.html.j2", form=form)
