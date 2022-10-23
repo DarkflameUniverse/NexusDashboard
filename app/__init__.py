3fa8bd46511b44af1bae6fa64713c1f2990df3ab
aronwk-aaron <aronwk.aaron@gmail.com>
2022-10-23 17:39:20 -0500

    fix handling new flag

--- app/__init__.py
+++ app/__init__.py
@@ -264,10 +264,11 @@ def register_settings(app):
         'USER_EMAIL_SENDER_EMAIL',
         app.config['USER_EMAIL_SENDER_EMAIL']
     )
-    app.config['ENABLE_CHAR_XML_UPLOAD'] = os.getenv(
-        'ENABLE_CHAR_XML_UPLOAD',
-        app.config['ENABLE_CHAR_XML_UPLOAD']
-    )
+    if "ENABLE_CHAR_XML_UPLOAD" in app.config:
+        app.config['ENABLE_CHAR_XML_UPLOAD'] = os.getenv(
+            'ENABLE_CHAR_XML_UPLOAD',
+            app.config['ENABLE_CHAR_XML_UPLOAD']
+        )
 
 
 def gm_level(gm_level):
